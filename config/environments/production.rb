2614f3230ded836dc15bc17f98faf9b4d4df43d1
Felipe Zingoni <felipe.z@aliada.mx>
2022-07-16 00:31:29 -0300

    modificaciones

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {host: 'https://zinmaseeds-app.herokuapp.com/'}
-  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
